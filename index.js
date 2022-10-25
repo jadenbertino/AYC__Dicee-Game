fed6911d954ea347480f403a50cb1eedc725257e
Jaden <jaden@bertinofamily.com>
2022-10-25 10:49:12 -0700

    Header responds to winner

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function chooseDice () {
+function chooseNum () {
   let randomNum = Math.ceil(Math.random() * 6);
-  let imgSrc = "./images/dice" + randomNum.toString() + ".png";
-  return imgSrc;
+  // let imgSrc = "./images/dice" + randomNum.toString() + ".png";
+  return randomNum;
 }
 
-function setRandomDice() {
-  let dice1 = document.querySelector(".player1__img");
-  dice1.setAttribute("src", chooseDice())
+function showDice(player1Roll, player2Roll) {
+  let dice1 = "./images/dice" + player1Roll.toString() + ".png";
+  document.querySelector(".player1__img").setAttribute("src", dice1);
 
-  let dice2 = document.querySelector(".player2__img");
-  dice2.setAttribute("src", chooseDice())
+  let dice2 = "./images/dice" + player2Roll.toString() + ".png";
+  document.querySelector(".player2__img").setAttribute("src", dice2);
+}
+
+function setWinner(player1, player2) {
+  if (player1 > player2) {
+    document.querySelector(".header").textContent = "🚩 Player 1 Wins!"
+  } else if (player1 == player2) {
+    document.querySelector(".header").textContent = "Tie Game!"
+  } else {
+    document.querySelector(".header").textContent = "Player 2 Wins! 🚩"
+  }
+  document.querySelector(".play-again").classList.add("play-again--show");
+}
+
+
+
+function diceRoll() {
+  let player1Num = chooseNum();
+  let player2Num = chooseNum();
+  showDice(player1Num, player2Num);
+  setWinner(player1Num, player2Num);
 }
